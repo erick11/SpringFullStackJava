537659d7baae3742cd6bc90b3b4d4cf74304fef7
Erick <Erick@DESKTOP-PLUO1AG>
2018-09-20 00:23:26 -0500

    - Se agrego a la clase IPersonaDAO JpaRepository<Persona, Integer>: Esta linea de codigo genera los metodos CRUD

--- 01_RESTDemo/src/main/java/com/mitocode/dao/IPersonaDAO.java
+++ 01_RESTDemo/src/main/java/com/mitocode/dao/IPersonaDAO.java
@@ -1,5 +1,14 @@
 package com.mitocode.dao;
 
-public interface IPersonaDAO extends {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.mitocode.model.Persona;
+
+
+public interface IPersonaDAO extends JpaRepository<Persona, Integer>{
+
+	/**
+	 JpaRepository<Persona, Integer>: Esta linea de codigo genera los metodos CRUD
+	 * 
+	 * */
 }
