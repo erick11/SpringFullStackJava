189380884c1b57840091d7ac45d5398a9e6810fc
Erick <Erick@DESKTOP-PLUO1AG>
2018-09-20 00:26:37 -0500

    - Se agrego el metodo  registrar

--- 01_RESTDemo/src/main/java/com/mitocode/controller/PersonaController.java
+++ 01_RESTDemo/src/main/java/com/mitocode/controller/PersonaController.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mitocode.model.Persona;
@@ -17,9 +20,11 @@ import com.mitocode.service.IPersonaService;
 /**
  * @RestController: nueva version
  * @Controller: antigua version
+   @RequestMapping: A la hora de consultar el servico antepones "persona"
  */
 
 @RestController
+@RequestMapping("/persona")
 public class PersonaController {
 
 	@Autowired
@@ -48,5 +53,21 @@ public class PersonaController {
 		
 		return new ResponseEntity<List<Persona>>(personas, HttpStatus.OK);
 	}
-
+	
+	@PostMapping(value = "/registrar", consumes = MediaType.APPLICATION_JSON_VALUE, produces= MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Integer> registrar(@RequestBody Persona persona){
+		/**
+		@RequestBody: indicar que la clase Persona sera de typo JSON
+		*/
+		
+		int resultado= 0;
+		try {
+			service.registrar(persona);
+			resultado = 1;
+		} catch (Exception e) {
+			new ResponseEntity<>(resultado, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<Integer>(resultado, HttpStatus.OK);
+	}
 }
