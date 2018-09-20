d0249d2d16196c003926f5ed04fe503c1960e192
Erick <Erick@DESKTOP-PLUO1AG>
2018-09-20 00:20:20 -0500

    - Se cambio a base de datos las consultas

--- 01_RESTDemo/src/main/java/com/mitocode/service/PersonaServiceImpl.java
+++ 01_RESTDemo/src/main/java/com/mitocode/service/PersonaServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mitocode.dao.IPersonaDAO;
 import com.mitocode.model.Persona;
 /**
  @Service = serive para indicar los servicos para la anotacion @Autowired	
@@ -15,40 +16,47 @@ import com.mitocode.model.Persona;
 @Service
 public class PersonaServiceImpl implements IPersonaService{
 
+	@Autowired
+	private IPersonaDAO dao; 
+	
+	
 	@Override
 	public void registrar(Persona per) {
-		// TODO Auto-generated method stub
 		
+		dao.save(per);
 	}
 
 	@Override
 	public void modificar(Persona per) {
-		// TODO Auto-generated method stub
+
+		dao.save(per);
+	}
+
+	@Override
+	public void eliminar(Integer id) {
+		dao.delete(id);
 		
 	}
 
 	@Override
-	public void eliminar(Persona per) {
-		// TODO Auto-generated method stub
+	public Persona listar(Integer id) {
 		
-	}
-
-	@Override
-	public Persona listar(int id) {
-
-		
-		return null;
+		return dao.findOne(id);
 	}
 
 	@Override
 	public List<Persona> listar() {
-		List <Persona> lista = new ArrayList<>();
-		lista.add(new Persona(1, "Erick Eduardo", "Navarrete Guerrero", 29));
-		lista.add(new Persona(2, "Ryan Joseph", "Navarrete Guerrero", 29));
-		lista.add(new Persona(3, "Cristiano Ronaldo", "dos Santos Aveiro", 29));
-		lista.add(new Persona(4, "Marco ", "Reus", 29));
+//		List <Persona> lista = new ArrayList<>();
+//		lista.add(new Persona(1, "Erick Eduardo", "Navarrete Guerrero", 29));
+//		lista.add(new Persona(2, "Ryan Joseph", "Navarrete Guerrero", 29));
+//		lista.add(new Persona(3, "Cristiano Ronaldo", "dos Santos Aveiro", 29));
+//		lista.add(new Persona(4, "Marco ", "Reus", 29));
+//		
+//		return lista;
 		
-		return lista;
+		return dao.findAll();
+	
 	}
+	
 
 }
