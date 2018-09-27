7fef394256e5477dd146f075eb60061b9c97eb6e
Erick <Erick@DESKTOP-PLUO1AG.home>
2018-09-27 04:24:47 -0500

    - Se corrigio los espacios

--- mediApp-backend/src/main/java/com/mitocode/model/ConsultaExamen.java
+++ mediApp-backend/src/main/java/com/mitocode/model/ConsultaExamen.java
@@ -23,8 +23,6 @@ public class ConsultaExamen {
 		this.idExamen = idExamen;
 	}
 
-
-
 	public Integer getIdConsulta() {
 		return idConsulta;
 	}
@@ -32,9 +30,59 @@ public class ConsultaExamen {
 		this.idConsulta = idConsulta;
 	}
 
+	public Integer getIdExamen() {
+		return idExamen;
+	}
 
 
+	public void setIdExamen(Integer idExamen) {
+		this.idExamen = idExamen;
+	}
 
+
+	
+	@Override
+	public String toString() {
+		return "ConsultaExamen [idConsulta=" + idConsulta + ", idExamen=" + idExamen + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idConsulta == null) ? 0 : idConsulta.hashCode());
+		result = prime * result + ((idExamen == null) ? 0 : idExamen.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConsultaExamen other = (ConsultaExamen) obj;
+		if (idConsulta == null) {
+			if (other.idConsulta != null)
+				return false;
+		} else if (!idConsulta.equals(other.idConsulta))
+			return false;
+		if (idExamen == null) {
+			if (other.idExamen != null)
+				return false;
+		} else if (!idExamen.equals(other.idExamen))
+			return false;
+		return true;
+	}
+
+	
+	
+
+	
 	
 
 
