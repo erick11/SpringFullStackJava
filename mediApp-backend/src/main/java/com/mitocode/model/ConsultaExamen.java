00113d5d8b5045828d28ca00d843e9484ad29260
Erick <Erick@DESKTOP-PLUO1AG.home>
2018-09-27 04:24:00 -0500

    - Se realizo el mapeo de la entidad ConsultaExamen

--- mediApp-backend/src/main/java/com/mitocode/model/ConsultaExamen.java
+++ mediApp-backend/src/main/java/com/mitocode/model/ConsultaExamen.java
@@ -1,22 +1,29 @@
 package com.mitocode.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity(name="tb_ConsultaExamen")
 public class ConsultaExamen {
-
+	
+	@Id
 	private Integer idConsulta;
-	private Integer Examen;
+	@Column(name="")
+	private Integer idExamen;
 	
 	public ConsultaExamen() {
 		
 	}
-
-	public ConsultaExamen(Integer idConsulta, Integer examen) {
-		super();
+	
+	
+	public ConsultaExamen(Integer idConsulta, Integer idExamen) {
+		
 		this.idConsulta = idConsulta;
-		Examen = examen;
+		this.idExamen = idExamen;
 	}
+
+
 
 	public Integer getIdConsulta() {
 		return idConsulta;
@@ -25,49 +32,11 @@ public class ConsultaExamen {
 		this.idConsulta = idConsulta;
 	}
 
-	public Integer getExamen() {
-		return Examen;
-	}
-	public void setExamen(Integer examen) {
-		Examen = examen;
-	}
+
+
 
 	
-	@Override
-	public String toString() {
-		return "ConsultaExamen [idConsulta=" + idConsulta + ", Examen=" + Examen + "]";
-	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((Examen == null) ? 0 : Examen.hashCode());
-		result = prime * result + ((idConsulta == null) ? 0 : idConsulta.hashCode());
-		return result;
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ConsultaExamen other = (ConsultaExamen) obj;
-		if (Examen == null) {
-			if (other.Examen != null)
-				return false;
-		} else if (!Examen.equals(other.Examen))
-			return false;
-		if (idConsulta == null) {
-			if (other.idConsulta != null)
-				return false;
-		} else if (!idConsulta.equals(other.idConsulta))
-			return false;
-		return true;
-	}
-	
 	
 }
