87908f05a2a6e95343f8912f9146ac096f91dc1c
Erick <Erick@DESKTOP-PLUO1AG>
2018-10-02 23:42:58 -0500

    - Creacion de la clase DAO para la clase Consulta

---
+++ mediApp-backend/src/main/java/com/mitocode/dao/IConsultaDao.java
@@ -0,0 +1,10 @@
+package com.mitocode.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mitocode.model.Consulta;
+
+public interface IConsultaDao extends JpaRepository<Consulta, Integer>{
+
+	
+}
