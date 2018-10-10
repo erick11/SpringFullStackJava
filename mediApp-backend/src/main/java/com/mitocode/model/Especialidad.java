0e22493b48e8171b376fdcce33e0f73c0506f8ff
enavarrete <enavarrete@ALGPRY005.ALIGNET.com>
2018-10-10 17:42:41 -0500

    - Se agrego 2 importaciones GeneratedValue, GenerationType

--- mediApp-backend/src/main/java/com/mitocode/model/Especialidad.java
+++ mediApp-backend/src/main/java/com/mitocode/model/Especialidad.java
@@ -2,6 +2,8 @@ package com.mitocode.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,9 +12,10 @@ import javax.persistence.Table;
 public class Especialidad {
 
 	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Integer idEspecialidad;
 	
-	@Column(name="")
+	@Column(name="nombre")
 	private String nombre;
 	
 	public Especialidad() {
