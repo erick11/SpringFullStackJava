908794c8e23fd89561c966c8ac39d360862af1ea
enavarrete <enavarrete@ALGPRY005.ALIGNET.com>
2018-10-10 21:13:08 -0500

    - Se agrego los metodos toString, hashCode y equals

--- mediApp-backend/src/main/java/com/mitocode/model/Especialidad.java
+++ mediApp-backend/src/main/java/com/mitocode/model/Especialidad.java
@@ -42,5 +42,40 @@ public class Especialidad {
 		this.nombre = nombre;
 	}
 
+	@Override
+	public String toString() {
+		return "Especialidad [idEspecialidad=" + idEspecialidad + ", nombre=" + nombre + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idEspecialidad == null) ? 0 : idEspecialidad.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Especialidad other = (Especialidad) obj;
+		if (idEspecialidad == null) {
+			if (other.idEspecialidad != null)
+				return false;
+		} else if (!idEspecialidad.equals(other.idEspecialidad))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
 	
 }
